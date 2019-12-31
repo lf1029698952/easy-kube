52780b568b486706c9a79396f989b6c292254e7c
lf1029698952 <3659384@163.com>
2019-12-31 14:22:00 +0800

    UPDATE：更新kube-controller-manager节点客户端证书签发有效期为10年并配置节点证书自动轮转，适用于k8s v1.8+

--- roles/k8s-master/templates/kubeconfig.sh
+++ roles/k8s-master/templates/kubeconfig.sh
@@ -62,3 +62,12 @@ kubectl config set-context kubernetes \
 
 # 设置默认上下文
 kubectl config use-context kubernetes
+
+# 自动批准 system:bootstrappers 组用户 TLS bootstrapping 首次申请证书的 CSR 请求
+kubectl create clusterrolebinding node-client-auto-approve-csr --clusterrole=system:certificates.k8s.io:certificatesigningrequests:nodeclient --group=system:bootstrappers
+
+# 自动批准 system:nodes 组用户更新 kubelet 自身与 apiserver 通讯证书的 CSR 请求
+kubectl create clusterrolebinding node-client-auto-renew-crt --clusterrole=system:certificates.k8s.io:certificatesigningrequests:selfnodeclient --group=system:nodes
+
+# 自动批准 system:nodes 组用户更新 kubelet 10250 api 端口证书的 CSR 请求
+kubectl create clusterrolebinding node-server-auto-renew-crt --clusterrole=system:certificates.k8s.io:certificatesigningrequests:selfnodeserver --group=system:nodes
